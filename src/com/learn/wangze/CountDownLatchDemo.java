e1981328cf37cc380c8f820fb56fa85e73226e7a
lijiale <1157221853@qq.com>
2021-07-08 12:53:53 +0800

    CountDownLatch

---
+++ src/com/learn/wangze/CountDownLatchDemo.java
@@ -0,0 +1,29 @@
+package com.learn.wangze;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Description:
+ * date: 2021/7/8 12:35
+ * Package: com.learn.wangze
+ *
+ * @author 李佳乐
+ * @email 18066550996@163.com
+ */
+@SuppressWarnings("all")
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+        for (int i = 1; i <= 10; i++) {
+            int finalI = i;
+            new Thread(new FutureTask<String>(() -> {
+                System.out.println(Thread.currentThread().getName() + "号线程离开");
+                countDownLatch.countDown();
+                return String.valueOf(finalI);
+            }), String.valueOf(i)).start();
+        }
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + "线程离开");
+    }
+}
